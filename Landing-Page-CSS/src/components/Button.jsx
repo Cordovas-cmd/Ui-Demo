602bdb082a494f8e4c9ad86da693d9df42a67fa6
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2024-04-05 12:04:05 -0500

    added some left sidebar styles

--- Landing-Page-CSS/src/components/Button.jsx
+++ Landing-Page-CSS/src/components/Button.jsx
@@ -2,12 +2,13 @@ import * as React from 'react';
 // import Button from '@mui/material/Button';
 
 export default function ButtonUsage() {
-  return <button variant="contained"
-  style={{
+  return <button 
+  // style={{
     
-    margin: "2px",
-    backgroundColor: "#e6ecf3",
-    color: "#1d2b34"
-  }}>Hello world</button>;
+  //   margin: "2px",
+  //   backgroundColor: "#e6ecf3",
+  //   color: "#1d2b34"
+  // }}
+  >Hello world</button>;
 }
 
